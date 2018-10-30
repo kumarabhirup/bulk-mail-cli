792b51b641fa48b8f243ba64c3f632fe3f8c28b1
Kumar Abhirup <kumar@iqubex.com>
2018-10-30 12:36:09 +0530

    ✏️ npm initialized

---
+++ src/bulkmailer.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @name bulk-mail-cli
+ * @description Send bulk non-spammy emails right from your terminal!
+ * @author Kumar Abhirup
+ * 
+ * @file bulkmailer.js
+ * This file is the entrypoint of this program.
+ * 
+ **/
