71bb2b10e0c0ba8cb787fee8d44be10a06d81154
Kumar Abhirup <kumar@iqubex.com>
2018-10-30 14:18:05 +0530

    💃 Boilerplate code added
    This commit enables the bot to run on a global installation of this package.

--- src/bulkmailer.js
+++ src/bulkmailer.js
@@ -8,3 +8,22 @@
  * This file is the entrypoint of this program.
  * 
  **/
+
+var args = process.argv.splice(process.execArgv.length + 2) // Retrieve all Arguments
+var terminal = require('terminal-kit').terminal
+var fs = require('fs')
+
+var config = fs.readFileSync(process.cwd() + '/.config/settings.json')
+var data = JSON.parse(config)
+
+var name = args[0] // Retrieve the first argument
+
+var newData = JSON.stringify(data, null, 2)
+fs.writeFile(process.cwd() + '/.config/settings.json', newData, function (err){
+    console.log(err)
+})
+terminal.green.bold('\n\nHi... ' + name + '! This is Bulkmail 😁\n\n\n')
+
+console.log(newData)
+console.log(args);
+
